ee272f67e983a0d6d9de4065c063e95072828f97
aomine <aomine1745@gmail.com>
2020-05-23 19:08:33 -0500

    v1.0

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const trailer = document.querySelector('.trailer')
+const video = document.querySelector('video')
+
+function toggle () {
+	trailer.classList.toggle('active')
+	video.pause()
+	video.currentTime = 0
+}
